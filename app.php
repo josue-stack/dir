8f92faa2405c12bf7684811462d2f2d813c409df
JosueCosta <contato_josuecosta@hotmail.com>
2020-06-18 17:29:49 -0300

    first commit

---
+++ app.php
@@ -0,0 +1,7 @@
+<?php
+require_once("./pages/header.php");
+?>
+<br>
+
+<hr>
+Inicio do site
---
+++ pages/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="templates/header.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            
+            <div class="container-logo">
+                <img src="" alt="">
+            </div>
+
+            <div class="container-menu">
+                <ul>
+                    <li><a href="pages/home.php"><img src="images/home.png" alt="">  Home </a></li>
+                    <li><a href="pages/gallery.php"><img src="images/gallery.png" alt="">  Galeria </a></li>
+                    <li><a href="pages/articles.php"><img src="images/articles.png" alt="">  Artigos </a></li>
+                    <li><a href="pages/contact.php"><img src="images/contact.png" alt="">  Contato </a></li>
+                     
+                </ul>
+            </div>
+        </div>
+    
+    </header>
+</body>
+</html>
